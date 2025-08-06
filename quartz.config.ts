cebf4d26493c20d6e07b63a3eb5e98479627bf1a
briannamagtoto <briannamagtoto@gmail.com>
2025-08-06 00:06:41 -0700

    Quartz sync: Aug 6, 2025, 12:06 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -10,7 +10,7 @@ const config: QuartzConfig = {
   configuration: {
     pageTitle: "aninternet.farm/notes",
     pageTitleSuffix: "",
-    enableSPA: true,
+    enableSPA: false,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
