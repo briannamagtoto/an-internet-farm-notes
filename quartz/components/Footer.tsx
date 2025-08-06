62121c511981517ebaf6537604b9b65bf311aae3
briannamagtoto <briannamagtoto@gmail.com>
2025-08-06 16:01:21 -0700

    Quartz sync: Aug 6, 2025, 4:01 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -1,6 +1,5 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import style from "./styles/footer.scss"
-import { version } from "../../package.json"
 import { i18n } from "../i18n"
 
 interface Options {
@@ -9,21 +8,31 @@ interface Options {
 
 export default ((opts?: Options) => {
   const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
-    const year = new Date().getFullYear()
     const links = opts?.links ?? []
+    const linkEntries = Object.entries(links)
+    const firstLink = linkEntries[0]
+    const remainingLinks = linkEntries.slice(1)
+    
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          {i18n(cfg.locale).components.footer.createdWith}
+          {firstLink && (
+            <>
+              {" "}
+              <a href={firstLink[1]}>{firstLink[0]}</a>
+            </>
+          )}
         </p>
-        <ul>
-          {Object.entries(links).map(([text, link]) => (
-            <li>
-              <a href={link}>{text}</a>
-            </li>
-          ))}
-        </ul>
+        {remainingLinks.length > 0 && (
+          <ul>
+            {remainingLinks.map(([text, link]) => (
+              <li>
+                <a href={link}>{text}</a>
+              </li>
+            ))}
+          </ul>
+        )}
       </footer>
     )
   }
--- quartz/i18n/locales/en-US.ts
+++ quartz/i18n/locales/en-US.ts
@@ -36,7 +36,7 @@ export default {
       title: "Explorer",
     },
     footer: {
-      createdWith: "Created with",
+      createdWith: "hand gardened by",
     },
     graph: {
       title: "Graph View",
