1b849523d039967afeca09a9d76c68057dc3c7d8
briannamagtoto <briannamagtoto@gmail.com>
2025-08-09 13:03:49 -0700

    Quartz sync: Aug 9, 2025, 1:03 PM

---
+++ quartz/plugins/transformers/folderMapping.ts
@@ -0,0 +1,47 @@
+import { QuartzTransformerPlugin } from "../types"
+import { FullSlug, slugifyFilePath, FilePath } from "../../util/path"
+import path from "path"
+
+export interface Options {}
+
+const defaultOptions: Options = {}
+
+export const FolderMapping: QuartzTransformerPlugin<Partial<Options>> = (userOpts) => {
+  const opts = { ...defaultOptions, ...userOpts }
+  
+  return {
+    name: "FolderMapping",
+    markdownPlugins() {
+      return [
+        () => {
+          return (tree, file) => {
+            // Check if frontmatter has publish-folder field
+            const publishFolder = file.data.frontmatter?.["publish-folder"]
+            
+            if (publishFolder && typeof publishFolder === "string") {
+              // Get the original filename without path
+              const basename = path.basename(file.data.relativePath as string, ".md")
+              
+              // Create new slug with custom folder
+              const newPath = path.posix.join(publishFolder, basename + ".md")
+              const customSlug = slugifyFilePath(newPath as FilePath)
+              
+              // Update the slug
+              file.data.slug = customSlug
+              
+              console.log(`Folder mapping: ${file.data.relativePath} -> ${customSlug}`)
+            }
+          }
+        }
+      ]
+    }
+  }
+}
+
+declare module "vfile" {
+  interface DataMap {
+    frontmatter: { [key: string]: unknown } & {
+      "publish-folder"?: string
+    }
+  }
+}
